14ea8c7c983e06fd7222ca08618f5b50f9dd228d
Rafał Lorenz <vardius@gmail.com>
2017-06-15 18:14:50 +1000

    cleanup rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
-import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
-import globals from "rollup-plugin-node-globals";
-import builtins from "rollup-plugin-node-builtins";
 import babel from "rollup-plugin-babel";
 import replace from "rollup-plugin-replace";
 import uglify from "rollup-plugin-uglify";
@@ -10,20 +7,16 @@ var env = process.env.NODE_ENV;
 var config = {
   format: "umd",
   moduleName: "peer-cdn",
-  exports: "named",
   plugins: [
+    resolve({
+      jsnext: true
+    }),
     babel({
       exclude: "node_modules/**"
     }),
     replace({
       "process.env.NODE_ENV": JSON.stringify(env)
-    }),
-    resolve({
-      jsnext: true
-    }),
-    globals(),
-    builtins(),
-    commonjs()
+    })
   ]
 };
 
