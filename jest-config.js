edf847c6563a606d6732a414f66fc642da8d451b
Rafał Lorenz <vardius@gmail.com>
2018-01-19 17:37:16 +1100

    Remove treshold

--- jest-config.js
+++ jest-config.js
@@ -3,14 +3,6 @@ module.exports = {
     'src/**/*.{js}',
     '!src/**/*.test.{js,jsx}',
   ],
-  coverageThreshold: {
-    global: {
-      statements: 98,
-      branches: 91,
-      functions: 98,
-      lines: 98,
-    },
-  },
   moduleDirectories: [
     'node_modules',
     'src',
