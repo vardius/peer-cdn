c30505581f7e76b4002c9e5c9d23c9910c0b13a8
Rafał Lorenz <vardius@gmail.com>
2018-01-29 20:03:49 +1100

    extract peer client

---
+++ src/clients/PeerClient.js
@@ -0,0 +1,106 @@
+import PeerData, { SocketChannel, EventDispatcher } from "peer-data";
+
+const defaults = {
+  servers: {
+    iceServers: [
+      {
+        // url: "stun:stun.1.google.com:19302"
+        url: "stun:74.125.142.127:19302"
+      }
+    ]
+  },
+  constraints: {
+    ordered: true
+  },
+  socket: {
+    jsonp: false
+  }
+};
+
+export const PeerEventType = { PEER: "PEER" };
+
+export default class PeerClient {
+  constructor(servers = defaults.servers, constraints = defaults.constraints, socket = defaults.socket, timeoutAfter = 1500) {
+    // Timeout after 1500 ms by default
+    this.timeoutAfter = timeoutAfter;
+    this.match = this.match.bind(this);
+    this.sendToRoom = this.sendToRoom.bind(this);
+    this.createRoomId = this.createRoomId.bind(this);
+
+    // setup peer client 
+    this.peerData = new PeerData(servers, constraints);
+    // setup signaling channel
+    this.signaling = new SocketChannel(socket);
+  }
+
+  match(request) {
+    return new Promise((resolve, reject) => {
+      const roomId = this.createRoomId();
+
+      const room = this.peerData.connect(roomId);
+
+      room.on("participant", function (participant) {
+        participant.then(peer => {
+          console.log('participant', peer);
+          peer.on("message", function (message) {
+            console.log('message', message);
+            if (!message) {
+              return;
+            }
+
+            // todo: handle chunk request
+            // https://github.com/vardius/peer-cdn/issues/7
+            room.disconnect();
+            resolve(message);
+          });
+
+          // renegotiate if there was an error
+          peer.on("error", function () { peer.renegotiate(); });
+        });
+      });
+
+      const url = new URL(request.url);
+      console.log('Ask for:', url.pathname, request);
+      EventDispatcher.getInstance().dispatch("send", {
+        type: PeerEventType.PEER,
+        caller: null,
+        callee: null,
+        room: { id: roomId },
+        data: url.pathname,
+      });
+
+      // Set up the timeout
+      setTimeout(() => {
+        room.disconnect();
+        reject('Promise timed out after ' + this.timeoutAfter + ' ms');
+
+      }, this.timeoutAfter);
+    });
+  }
+
+  sendToRoom(roomId, response) {
+    console.log('Connect to:', roomId);
+    // signaling server needs us to seed
+    // we will connected to a given room
+    const room = this.peerData.connect(roomId);
+    room.on("participant", participant => participant.then(function (peer) {
+      console.log('participant', peer);
+      console.log('response', response);
+      //this peer disconnected from room
+      peer.on("disconnected", function () { room.disconnect() });
+      // send the response
+      peer.send(response);
+    }));
+  }
+
+  createRoomId() {
+    let dt = new Date().getTime();
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      let r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+
+    return uuid;
+  }
+}
--- src/plugins/Peer.js
+++ src/plugins/Peer.js
@@ -1,47 +1,27 @@
-import PeerData, { SocketChannel, EventDispatcher } from "peer-data";
-
-const PeerEventType = { PEER: "PEER" };
-const defaults = {
-  servers: {
-    iceServers: [
-      {
-        // url: "stun:stun.1.google.com:19302"
-        url: "stun:74.125.142.127:19302"
-      }
-    ]
-  },
-  constraints: {
-    ordered: true
-  },
-  socket: {
-    jsonp: false
-  }
-};
+import { EventDispatcher } from "peer-data";
+import PeerClient, { PeerEventType } from "../clients/PeerClient";
 
 export default class Peer {
   constructor(options) {
-    // cahce name to get the response from
-    this.cacheName = options.cacheName;
-    // Timeout after 1500 ms by default
-    this.timeoutAfter = options.timeoutAfter || 1500;
-    this.getMiddleware = this.getMiddleware.bind(this);
-    this._match = this._match.bind(this);
+    if (!options) {
+      throw new Error('cacheName option is required');
+    }
 
-    // setup peer client 
-    this.peerData = new PeerData(
-      options.servers || defaults.servers,
-      options.constraints || defaults.constraints
-    );
-    // setup signaling channel
-    this.signaling = new SocketChannel(
-      options.socket || defaults.socket
-    );
+    const { cacheName, servers, constraints, socket, timeoutAfter } = options;
+
+    // cahce name to get the response from
+    this.cacheName = cacheName;
+    this.client = new PeerClient(servers, constraints, socket, timeoutAfter);
+
+    this.getMiddleware = this.getMiddleware.bind(this);
 
     EventDispatcher.getInstance().register(PeerEventType.PEER, this._onPeerRequest.bind(this));
   }
 
   // Middleware factory function for fetch event
-  getMiddleware(request) {
+  getMiddleware(event) {
+    const request = event.request.clone();
+
     return {
       get: async () => {
         // do not cache ranged responses
@@ -51,74 +31,29 @@ export default class Peer {
         }
 
         try {
-          // this._match() will look for an entry in all of the peers available to the service worker.
-          const response = await this._match(request);
+          // this.match() will look for an entry in all of the peers available to the service worker.
+          const response = await this.client.match(request);
           if (response) {
             return response;
           }
 
           return null;
         } catch (e) {
+          console.log('PeerPlugin Error', e);
           return null;
         }
       }
     };
   }
 
-  _match(request) {
-    return new Promise((resolve, reject) => {
-      const roomId = '';
-      const room = this.peerData.connect(roomId);
-
-      room.on("participant", function (participant) {
-        participant.then(peer => {
-          peer.on("message", function (payload) {
-            if (!payload) {
-              return;
-            }
-
-            // todo: handle chunk request
-            // https://github.com/vardius/peer-cdn/issues/7
-            room.disconnect();
-            resolve(payload);
-          });
-
-          // renegotiate if there was an error
-          peer.on("error", function () { peer.renegotiate(); });
-        });
-      });
-
-      const url = new URL(request.url);
-      EventDispatcher.getInstance().dispatch("send", {
-        type: PeerEventType.PEER,
-        caller: null,
-        callee: null,
-        room: { id: roomId },
-        data: url.pathname,
-      });
-
-      // Set up the timeout
-      setTimeout(() => {
-        room.disconnect();
-        reject('Promise timed out after ' + this.timeoutAfter + ' ms');
-      }, this.timeoutAfter);
-    });
-  }
-
   _onPeerRequest(e) {
+    console.log('_onPeerRequest', e)
     // caches.match() will look for a cache entry in all of the caches available to the service worker.
     caches.open(this.cacheName).then(cache => {
       cache.match(e.data).then(response => {
+        console.log('cache.match', response);
         if (response) {
-          // signaling server needs us to seed
-          // we will connected to a given room
-          const room = this.peerData.connect(e.room.id);
-          room.on("participant", participant => participant.then(function (peer) {
-            //this peer disconnected from room
-            peer.on("disconnected", function () { room.disconnect() });
-            // send the response
-            peer.send(response);
-          }));
+          this.client.sendToRoom(e.room.id, response);
         }
       });
     });
