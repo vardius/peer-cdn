61a179dd179e4437a7c1866ddda34c9d124eb11e
Rafał Lorenz <vardius@gmail.com>
2018-01-29 20:02:47 +1100

    use delegate plugin

--- example/sw.js
+++ example/sw.js
@@ -1,32 +1,21 @@
 (function () {
   "use strict";
 
-  self.importScripts("/vendor/peer-cdn.min.js");
+  self.importScripts("/vendor/peer-cdn.js");
 
   const cachePlugin = new CachePlugin({ version: 1 });
-  const peerPlugin = new PeerPlugin({
-    // we pass the cache name to peer plugin
-    // otherwise it will not get a cached response
-    cacheName: cachePlugin.names.peerfetch,
-    servers: {
-      iceServers: [
-        {
-          url: "stun:74.125.142.127:19302"
-        }
-      ]
-    },
-    constraints: {
-      ordered: true
-    },
-  });
+  // since sw does not support webrtc yet we use PeerPlugin on client side 
+  // and we delegate request to it with DelegatePlugin
+  const delegatePlugin = new DelegatePlugin({ timeoutAfter: 5000 });
   const networkPlugin = new NetworkPlugin();
 
   function run() {
     const cdn = new PeerCDN();
-    cdn.all("/", STRATEGIES.ordered,
+    cdn.GET("/", STRATEGIES.ordered,
+      // cdn.GET("/js/main.js", STRATEGIES.ordered,
       // all of the following plugins are required
       cachePlugin.getMiddleware,
-      peerPlugin.getMiddleware,
+      delegatePlugin.getMiddleware,
       networkPlugin.getMiddleware
     );
     cdn.register();
