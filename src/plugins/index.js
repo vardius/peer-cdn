9ad32087410b76a128b980a2b94fedb70ef31433
Rafał Lorenz <vardius@gmail.com>
2018-01-29 20:05:25 +1100

    export delegate plugin

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,6 @@
 import Cache from "./Cache";
+import Delegate from "./Delegate";
 import Peer from "./Peer";
 import Network from "./Network";
 
-export { Cache, Peer, Network };
+export { Cache, Delegate, Peer, Network };
